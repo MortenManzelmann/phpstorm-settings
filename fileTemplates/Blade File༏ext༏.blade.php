3a0466cdaa2656cc3a08b6b802facc6fd6a5b59b
Morten Manzelmann <morten.manzelmann@ministrygroup.de>
2018-10-16 11:11:14 +0200

    PS-2018.1.6 <Morten.Manzelmann@Morten-PC Create baseRefactoring.xml, markdown.xml, IntelliLang.xml, ide.general.xml, git.xml, project.default.xml, Default copy.xml, other.xml, terminal.xml, Default.xml, Blade File Header.php, find.xml, ui.lnf.xml, filetypes.xml, php.xml, keymap.xml, remote-servers.xml, colors.scheme.xml, databaseDrivers.xml, laf.xml, Sass File.sass, PHP File Header.php, databaseSettings.xml, Blade File༏ext༏.blade.php, debugger.xml

---
+++ fileTemplates/Blade File༏ext༏.blade.php
@@ -0,0 +1,1 @@
+#parse("Blade File Header.php")
---
+++ fileTemplates/includes/Blade File Header.php
@@ -0,0 +1,8 @@
+{{--
+/**
+* Project: ${PROJECT_NAME}
+* User: Morten Manzelmann <morten.manzelmann@napsys.de>
+* Date: ${DATE} - ${TIME}
+* Copyright by Napsys GmbH
+*/
+--}}
